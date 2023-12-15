0c9ab832bde306701e55b853e7d4353512d97ebf
HawkingHuang <math0915@gmail.com>
2023-12-15 22:15:14 +0800

    Finish experience section

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -3,19 +3,50 @@
     <TabGroup>
       <TabList class="tab-list">
         <Tab class="tab">簡介</Tab>
-        <Tab class="tab">學歷／經歷</Tab>
+        <Tab class="tab">學經歷</Tab>
         <Tab class="tab">主要作品</Tab>
         <Tab class="tab">技術能力</Tab>
         <Tab class="tab">自傳</Tab>
       </TabList>
       <TabPanels>
         <TabPanel class="tab-panel">Content 1</TabPanel>
-        <TabPanel class="tab-panel">Content 2</TabPanel>
         <TabPanel class="tab-panel">
-          <h2>
+          <div class="exp-section">
+            <h2 class="exp-section-title">學歷</h2>
+            <p class="exp-section-text">國立高雄師範大學 英語學系</p>
+          </div>
+          <div class="exp-section">
+            <h2 class="exp-section-title">經歷</h2>
+            <p class="exp-section-text">
+              興巨資訊股​份有限公司 - Meta 筆譯專員（2019 年 6 月至 2022 年 3
+              月）
+            </p>
+            <p class="exp-section-text-detail">
+              - 主要負責處理 Meta（前身為 Facebook）
+              的文件翻譯，領域涵蓋使用說明、遊戲、部落格、行銷等。
+            </p>
+            <p class="exp-section-text">
+              自由譯者（2022 年 4 月至 2023 年 8 月）
+            </p>
+            <p class="exp-section-text-detail">- 全職擔任自由譯者</p>
+            <p class="exp-section-text">
+              創科資訊股份有限公司 - 實習生（2023 年 9 月至 2023 年 10 月）
+            </p>
+            <p class="exp-section-text-detail">
+              - 實習過程前兩週主要是對 Git、Figma、Nginx、Ubuntu
+              等技術有初步的認識，並完成相關作業。過程中也因為得知該公司主要是使用
+              Vue 和 Laravel 進行開發，因此在原本就有 JS 基礎的情況下開始學習
+              Vue，並於 10
+              月左右開始參與該公司「Monospace」共同工作空間的會員系統維護工作。
+            </p>
+          </div>
+        </TabPanel>
+        <TabPanel class="tab-panel">
+          <h2 class="portfolio">
             <ion-icon name="caret-forward-outline"></ion-icon
             ><ion-icon name="caret-forward-outline"></ion-icon
-            ><a href="http://localhost:8085/" class="portfolio">點擊檢視作品</a
+            ><a href="http://localhost:8085/" class="portfolio-link"
+              >點擊檢視作品</a
             ><ion-icon name="caret-back-outline"></ion-icon
             ><ion-icon name="caret-back-outline"></ion-icon>
           </h2>
@@ -23,8 +54,8 @@
             <div class="portfolio-intro-section">
               <h3 class="portfolio-intro-title">作品簡介</h3>
               <p>
-                Traditional Chinese
-                Checker是一項繁體中文錯字檢查工具，之所以想開發此工具是因為之前從事翻譯工作時，交稿前都會進行錯字校正，但由於市面上中文語系的相關工具寥寥可數，加上介面和功能相對陽春，因此當初擬定題目時，才有了開發此工具的念頭產生。
+                Traditional Chinese Checker
+                是一項繁體中文錯字檢查工具，之所以想開發此工具是因為之前從事翻譯工作時，交稿前都會進行錯字校正，但由於市面上中文語系的相關工具寥寥可數，加上介面和功能相對陽春，因此當初擬定題目時，才有了開發此工具的念頭產生。
               </p>
             </div>
             <div class="portfolio-intro-section">
@@ -34,9 +65,10 @@
                 <ion-icon name="logo-firebase"></ion-icon> （實作 Router 與
                 Vuex）
               </p>
-              <p>具備RWD響應式網頁設計概念</p>
+              <p>具備 RWD 響應式網頁設計概念</p>
               <p>
-                針對主要路由使用KeepAlive和Transition元件，使用者在轉至其他路由並返回時，其輸入的內容將會保留，且過程中會有動畫效果
+                針對主要路由使用 KeepAlive 和 Transition
+                元件，使用者轉至其他路由並返回時，輸入的內容將會保留，且過程中會有動畫效果
               </p>
               <p>
                 註冊與登入頁面實作正規表達式驗證，使用者在輸入不符合格式的帳號密碼時，系統將會給予提示
@@ -90,7 +122,7 @@ export default {
 .tab-list {
   display: flex;
   justify-content: center;
-  margin: 3rem;
+  margin: 2rem;
   gap: 2rem;
 }
 
@@ -115,11 +147,44 @@ export default {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
 }
 
-.tab-panel h2 {
+/* Experience */
+.exp-section {
+  padding: 1rem;
+}
+
+.exp-section-title {
+  font-size: 2.4rem;
+  padding: 0.5rem;
+  margin: 0.5rem;
+  color: #1c7ed6;
+  text-align: left;
+}
+
+.exp-section-text {
+  font-size: 1.8rem;
+  font-weight: bold;
+  padding: 0.5rem;
+  margin: 0.5rem;
+  line-height: 1.8;
+  text-align: left;
+}
+
+.exp-section-text-detail {
+  color: #343a40;
+  font-size: 1.6rem;
+  font-weight: bold;
+  padding: 0.5rem;
+  margin: 0.5rem;
+  line-height: 1.5;
+  text-align: left;
+}
+
+/* Portfolio */
+.portfolio {
   padding: 2rem;
 }
 
-.portfolio {
+.portfolio-link {
   color: #fff;
   background-color: #1c7ed6;
   padding: 1rem;
@@ -130,12 +195,12 @@ export default {
 }
 
 .portfolio-intro-section {
-  margin: 2rem auto;
+  margin: 1.5rem auto;
 }
 
 .portfolio-intro-title {
   font-size: 2.4rem;
-  margin: 1rem;
+  margin: 0.5rem;
   color: #1c7ed6;
 }
 
